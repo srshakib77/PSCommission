a05fccf51ad01e6bfdc8494448dae1057b69ef62
Tasmia <2331183@iub.edu.bd>
2025-08-02 20:37:21 +0600

    int

---
+++ src/main/java/com/example/pscommission/Tasmia/CandidateDashboardController.java
@@ -0,0 +1,40 @@
+package com.example.pscommission.Tasmia;
+
+public class CandidateDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void seeresultsbuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void printcertificatebuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkexamdatebuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadadmitcardbuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void payfeesbuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applyonlinebuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createaccountbuttononaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewsexambuttononaction(ActionEvent actionEvent) {
+    }
+}
