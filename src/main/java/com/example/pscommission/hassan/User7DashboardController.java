2e3ecfa3f5c4ec2a51ea353c7b79a1609accf257
HassanMahmud256 <2310984@iub.edu.bd>
2025-08-11 15:16:24 +0600

    int

---
+++ src/main/java/com/example/pscommission/hassan/User7DashboardController.java
@@ -0,0 +1,24 @@
+package com.example.pscommission.hassan;
+
+public class User7DashboardController
+{
+    @javafx.fxml.FXML
+    private Button btnGoal6;
+    @javafx.fxml.FXML
+    private Button btnGoal5;
+    @javafx.fxml.FXML
+    private Button btnGoal4;
+    @javafx.fxml.FXML
+    private Button btnGoal3;
+    @javafx.fxml.FXML
+    private Button btnGoal2;
+    @javafx.fxml.FXML
+    private Button btnGoal1;
+    @javafx.fxml.FXML
+    private Button btnGoal8;
+    @javafx.fxml.FXML
+    private Button btnGoal7;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
